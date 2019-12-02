fafe725e401e211f340993e3d56283e22b688a08
Thomas Canty <thomas.m.canty@gmail.com>
2019-12-02 00:31:38 -0500

    Completed Work, Removed Old Sect 3

---
+++ .ipynb_checkpoints/SB_CAP_fcns-checkpoint.py
@@ -0,0 +1,83 @@
+'''
+Starbucks Capstone Project - Functions
+
+This file contains all of the larger functions used in the Starbucks Capstone Notebook, to keep the notebook cleaner and easier to read.
+
+'''
+## Import packages
+
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MultiLabelBinarizer
+import time
+
+def organize_raw_data(portfolio,profile,transcript):
+    ''' This function organizes the raw data via setting datatype, setting indexes, and onehotencoding
+    input: Startbuck challenge raw dataframes: portfolio,profile, and transcrips
+    
+    output: (7 dataframes, 3 revised originals and 4 which are filtered versions of transcript)
+    portfolio_new: portfolio (one hot encoded channels, some column renames)
+    profile_new: cleaned profile (member start date to datetime, one hot encoded gender)
+    transcript_new: cleaned transcript 
+        (value dict-> one hot encoded event type, 
+        amount(spend), reward(completion), offer_id(offers,view/complte))
+    t_offer: transcript_new filtered:offers only
+    t_viewed: transcript_new filtered:viewed only
+    t_trans: transcript_new filtered:transactions only
+    t_compl: transcript_new filtered:complteted offers only
+    
+    '''
+    
+    
+    ############### Portfolio
+    portfolio.rename(columns={'id':'offer_id'},inplace=True) # match other table
+    
+    # one hot encode channels
+    mlb = MultiLabelBinarizer() 
+    portfolio = portfolio.join(pd.DataFrame(mlb.fit_transform(portfolio.pop('channels')),
+                          columns=mlb.classes_,
+                          index=portfolio.index))
+    portfolio = portfolio.merge(pd.get_dummies(portfolio['offer_type'],prefix='offer',prefix_sep='_'),left_index=True,right_index=True)
+    #portfolio.drop(columns='offer_type',inplace=True)
+    # Since offer_id is unique, we can use that as an index
+    portfolio.set_index('offer_id')
+    
+    ############### Profile
+    #Split/Drop N/A  - approx 13% of profile data was missing gender and income, the rows missing gender also are missing income.
+    #  Will look to see if there are any differences later on
+    profile_na = profile[profile.gender.isna()]
+    #profile = profile.dropna(axis = 0)  - Will be doing this later so the profile N/A lines will be processed
+    
+    #Convert to datetime
+    profile.loc[:,'became_member_on']=pd.to_datetime(profile['became_member_on'],format='%Y%m%d')
+    
+    #one hot encode gender
+    profile = profile.merge(pd.get_dummies(profile['gender'],prefix='gender',prefix_sep='_'),left_index=True,right_index=True)
+    
+    #provides a relative length of membership based on the latest membership join.
+    profile['memb_leng'] = (profile.became_member_on.max()-profile.became_member_on).dt.days.astype(float)/(365.25/12)
+    
+    # Since id is unique, we can use that as an index
+    profile.set_index('id',inplace=True)
+    
+    ################# Transcript
+    
+    # expand dict in 'value' to its own columns per dict key
+    transcript = transcript.merge(transcript.value.apply(pd.Series),left_index=True,right_index=True)
+    
+    # cleaning up 'offer_id' and 'offer id', so as there is a single column
+    transcript.loc[transcript['offer_id'].isna(),'offer_id'] = transcript[transcript['offer_id'].isna()]['offer id']
+    
+    #one hot encode event (transaction type)
+    transcript = transcript.merge(pd.get_dummies(transcript['event'],prefix='event',prefix_sep='_'),left_index=True, right_index=True)
+    transcript.drop(columns = ['offer id','value'],inplace=True)
+    
+    transcript = transcript[transcript['person'].isin(profile.index)].reset_index()
+    
+    #break apart into sub-df based on transaction tyle
+    t_offer = transcript[transcript['event']=='offer received']
+    t_viewed = transcript[transcript['event']=='offer viewed']
+    t_trans = transcript[transcript['event']=='transaction']
+    t_compl = transcript[transcript['event']=='offer completed']
+    
+    return portfolio,profile,profile_na,transcript,t_offer,t_viewed,t_trans,t_compl
